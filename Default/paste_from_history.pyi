bd61e9d076a50063dd3eddf4650c75bfa6047f24
Gerard Roche <gerardroche@users.noreply.github.com>
2020-09-23 15:52:50 +0100

    Updates

--- Default/paste_from_history.pyi
+++ Default/paste_from_history.pyi
@@ -1,10 +1,6 @@
+g_clipboard_history = ...  # type: ClipboardHistory
+
+
 class ClipboardHistory:
-
-    def __init__(self) -> None:
-        ...
-
-    def push_text(self, text: str) -> None:
-        ...
-
-
-g_clipboard_history = ClipboardHistory()
+    def __init__(self) -> None: ...
+    def push_text(self, text: str) -> None: ...
